5529016b8e56ad5b0de1d2ce09559f94e3faa6b1
matias <diufram007@gmail.com>
2024-11-14 13:17:54 -0400

    Ya

--- utils/config.py
+++ utils/config.py
@@ -1,3 +1,3 @@
 # config.py
 
-OPENAI_API_KEY = "Bearer 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"
+OPENAI_API_KEY = ""
